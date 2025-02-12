a12d93855ee0dc6ff119795b52026dfb8ccedb31
Fynn <fynnruebenach@gmail.com>
2025-02-12 22:37:45 +0100

    Bump version

--- src/entities/Oembed.ts
+++ src/entities/Oembed.ts
@@ -8,7 +8,11 @@ export class Oembed {
    * Gets the Oembed for a track, playlist, or user.
    */
   public get = async (params: SoundcloudOembedFilter) => {
-    const response = await this.api.getWebsite("/oembed", params);
+    const response = await this.api.getWebsite(
+      "/oembed",
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      params as Record<string, any>,
+    );
     return response as Promise<SoundcloudOembed>;
   };
 }
